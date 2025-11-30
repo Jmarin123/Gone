c78f84b37e153add42ba10758a28feb1aeda3e95
Jay <waterdude50@gmail.com>
2025-11-30 02:31:06 -0500

    Feat: starting to incorperate cunity

---
+++ tests/test_lexer.c
@@ -0,0 +1,21 @@
+#include "unity.h"
+#include "lexer.h"
+
+void setUp() {
+
+}
+
+void tearDown() {
+
+}
+
+void simple_lexer(){
+    return;
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(simple_lexer);
+    UNITY_END();
+    return 0;
+}
